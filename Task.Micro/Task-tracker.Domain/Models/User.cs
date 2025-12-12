676c62ce138c1ed7ba2b8ca0f326a22026394357
Grrline <yaroslavefimovgg@gmail.com>
2025-12-12 14:30:14 +0500

    Создание модели юзера

---
+++ Task.Micro/Task-tracker.Domain/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_tracker.Domain.Models
+{
+    public class User : Entity
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
